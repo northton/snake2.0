924496599f3d1915219be548d7d6c845d47dd6f0
northton <148852707+northton@users.noreply.github.com>
2023-10-26 14:12:31 -0400

    Finish

--- food.js
+++ food.js
@@ -1,7 +1,14 @@
-let food = { x: 0, y: 0 }
+import { onSnake, expandSnake } from "./snake.js"
+import { randomGridPosition } from "./grid.js"
+
+let food = getRandomFoodPosition()
 const expansionRate = 1
 
 export function update () {
+    if (onSnake(food)) {
+        expandSnake(expansionRate)
+        food = getRandomFoodPosition()
+    }
 }
 
 export function draw (gameBoard) {
@@ -10,4 +17,12 @@ export function draw (gameBoard) {
     foodElement.style.gridColumnStart = food.x //Row
     foodElement.classList.add("food")
     gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
 }
--- game.js
+++ game.js
@@ -1,10 +1,19 @@
-import { update as updateSnake, draw as drawSnake, snakeSpeed } from "./snake.js"
+import { update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, snakeIntersection } from "./snake.js"
 import { update as updateFood, draw as drawFood } from "./food.js"
+import { outsideGrid } from "./grid.js"
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById("game-board")
 
 function main (currentTime) {
+    if(gameOver) {
+        if (confirm("You lost. Press OK to restart.")) {
+            window.location = "/"
+        }
+        return
+    }
+
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / snakeSpeed) {return}
@@ -21,10 +30,15 @@ window.requestAnimationFrame(main);
 function update() {
     updateFood()
     updateSnake()
+    checkFailure()
 }
 
 function draw() {
     gameBoard.innerHTML = ""
     drawFood(gameBoard)
     drawSnake(gameBoard)
+}
+
+function checkFailure() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
