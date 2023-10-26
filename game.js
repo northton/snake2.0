5f13959502e39d886b60eaf07c98950f94867385
northton <148852707+northton@users.noreply.github.com>
2023-10-26 07:06:35 -0400

    Created snake, food and some other things also

---
+++ game.js
@@ -0,0 +1,30 @@
+import { update as updateSnake, draw as drawSnake, snakeSpeed } from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js"; 
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board");
+
+function main (currentTime) {
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / snakeSpeed) {return};
+
+    console.log("Render");
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+    updateFood();
+    updateSnake();
+}
+
+function draw() {
+    gameBoard.innerHTML = "";
+    drawFood(gameBoard);
+    drawSnake(gameBoard);
+}
