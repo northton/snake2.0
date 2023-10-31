f8f4afb14ab27555e4e62e53984df340e4f40613
northton <148852707+northton@users.noreply.github.com>
2023-10-31 08:07:57 -0400

    add timer

--- game.js
+++ game.js
@@ -3,18 +3,25 @@ import { update as updateFood, draw as drawFood } from "./food.js"
 import { outsideGrid } from "./grid.js"
 
 let request
+let s = 0
 let gameOver = false
 const gameBoard = document.getElementById("game-board")
 let pause = document.getElementById("pause")
 let start = document.getElementById("start")
+let timer = document.getElementById("start")
 let i = 0
 let score = document.getElementById("scorejs")
 let time = document.getElementById("timejs")
-const now = new Date();
 // const dialog = document.querySelector("dialog")
 // const restart = document.getElementById("restart")
 // const quit = document.getElementById("quit")
 
+timer.addEventListener("click", () => {
+    setInterval(() => {
+        time.innerHTML = "00:" + s
+        s++
+    }, 1000)
+})
 
 start.addEventListener("click", () => {
     request = setInterval(() => {gameLoop()}, 300)
@@ -22,6 +29,7 @@ start.addEventListener("click", () => {
 
 pause.addEventListener("click", () => {
     clearInterval(request)
+    clearInterval(startTimer())
     if(confirm("The game is paused!Press OK to resume")) {
         request = setInterval(() => {gameLoop()}, 300)
     } else {
@@ -58,6 +66,10 @@ function draw() {
 function checkFailure() {
     gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
+
+
+
+
 
 
 
